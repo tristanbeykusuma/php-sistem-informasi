a0d5048e388b3ce217d2423b10254aa109fbab54
Tristan Kusuma <91164364+tristanbeykusuma@users.noreply.github.com>
2022-09-14 07:57:36 +0800

    database update
    database

--- koneksi/functions.php
+++ koneksi/functions.php
@@ -1,9 +1,9 @@
 <?php
 
-$server   ="localhost" ; //server localhost
-$username ="root"; //username default 
-$password =""; //password root. Default kosongkan aja
-$database   ="php_database"; //ini adalah nama database
+$server   ="sql6.freesqldatabase.com" ; //server localhost
+$username ="sql6519513"; //username default 
+$password ="xcmvhVFGC7"; //password root. Default kosongkan aja
+$database   ="sql6519513"; //ini adalah nama database
 $pdo = mysqli_connect($server, $username, $password, $database) or die("Connection failed: " . mysqli_connect_error());
 
 
