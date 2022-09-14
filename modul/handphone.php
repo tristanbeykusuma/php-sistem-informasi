4ab70bf0cf6fca6ec395318b6b934b2dcab242d4
Tristan Kusuma <91164364+tristanbeykusuma@users.noreply.github.com>
2022-09-14 08:28:45 +0800

    Update handphone.php

--- modul/handphone.php
+++ modul/handphone.php
@@ -1,8 +1,8 @@
 <?php
-$server   ="localhost" ; //server localhost
-$username ="root"; //username default 
-$password =""; //password root. Default kosongkan aja
-$database   ="php_database"; //ini adalah nama database
+$server   ="sql6.freesqldatabase.com" ; //server localhost
+$username ="sql6519513"; //username default 
+$password ="xcmvhVFGC7"; //password root. Default kosongkan aja
+$database   ="sql6519513"; //ini adalah nama database
 $conn = mysqli_connect($server, $username, $password, $database) or die("Connection failed: " . mysqli_connect_error());
 switch($_GET['aksi']){
 case "tampil": //untuk mengakses aksi=tampil
@@ -57,5 +57,5 @@ break;
 
 <body>
     <br></br>
-    <button onclick="window.open('http://digital-dental-record.herokuapp.com/modul/pdfoutput.php')"> Print Seluruh Data </button>
+    <button onclick="window.open('modul/pdfoutput.php')"> Print Seluruh Data </button>
 </body>
